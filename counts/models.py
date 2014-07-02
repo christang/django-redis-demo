140e80b4af0acc27df2c79771d9cd124cb8496af
Chris Tang <chris.l.tang@gmail.com>
2014-07-01 20:43:56 -0400

    Initial attempt to lock rebuild_cache
    If the cache is missing the keys holding cities and users, a rebuild
    is triggered that takes a non-trivial amount of time. If multiple
    users trigger rebuild simultaneously, this can cause a thundering
    herd on the table. To prevent this, there is a lock that allows
    only one user to rebuild the cache while others drop their requests.
    
    Depending on needs, this dropping behavior can be modified to
    block or wait on the rebuild to complete.

--- counts/models.py
+++ counts/models.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.db import models
 from django.core import cache
 
@@ -8,6 +10,7 @@ __all__ = ['Messages']
 prefix = 'messages'
 users_key = '%s:%s' % (prefix, 'users')
 cities_key = '%s:%s' % (prefix, 'cities')
+multiple_requests = 'multiple requests to rebuild cache, returning rather than waiting'
 
 
 class Messages(models.Model):
@@ -32,37 +35,77 @@ class Messages(models.Model):
                           force_update=force_update,
                           using=using,
                           update_fields=update_fields)
-        put_cache(self.state, self.city, self.username)
+        cache_contention = put_cache(self.state, self.city, self.username)
+        if cache_contention:
+            print "Cache missed an update since it was rebuilding"
 
     @staticmethod
     def get_city_count():
-        rebuild_city_cache()
-        return cache.cache.raw_client.scard(cities_key)
+        if rebuild_city_cache():
+            return cache.cache.raw_client.scard(cities_key)
+        else:
+            raise KeyError(multiple_requests)
 
     @staticmethod
     def get_user_count():
-        rebuild_user_cache()
-        return cache.cache.raw_client.scard(users_key)
+        if rebuild_user_cache():
+            return cache.cache.raw_client.scard(users_key)
+        else:
+            raise KeyError(multiple_requests)
+
+    @staticmethod
+    def flush_cache():
+        cache.cache.raw_client.delete(cities_key)
+        cache.cache.raw_client.delete(users_key)
 
 
 def put_cache(state, city, username):
-    rebuild_city_cache()
-    cache.cache.raw_client.sadd(cities_key, '%s:%s' % (state, city))
-    rebuild_user_cache()
-    cache.cache.raw_client.sadd(users_key, username)
+    cache_contention = False
+    if rebuild_city_cache():
+        cache.cache.raw_client.sadd(cities_key, '%s:%s' % (state, city))
+    else:
+        cache_contention = True
+
+    if rebuild_user_cache():
+        cache.cache.raw_client.sadd(users_key, username)
+    else:
+        cache_contention = True
+    return cache_contention
 
 
 def rebuild_city_cache():
-    if not cache.cache.raw_client.exists(cities_key):
+    if cache.cache.raw_client.exists(cities_key):
+        return True
+    lock = cities_key + '_lock'
+    token = new_token()
+    if cache.cache.raw_client.set(lock, token, 'px', 60000, 'nx'):
         q = Messages.objects.values('state', 'city').order_by('state', 'city').distinct('state', 'city')
-        cache.cache.raw_client.sadd(cities_key, *[state_city(dct) for dct in q])
+        if q:
+            cache.cache.raw_client.sadd(cities_key, *[state_city(dct) for dct in q])
+        cache.cache.raw_client.delete(lock)
+        return True
+    else:
+        return False
 
 
 def rebuild_user_cache():
-    if not cache.cache.raw_client.exists(users_key):
+    if cache.cache.raw_client.exists(users_key):
+        return True
+    lock = users_key + '_lock'
+    token = new_token()
+    if cache.cache.raw_client.set(lock, token, 'px', 60000, 'nx'):
         q = Messages.objects.values('username').order_by('username').distinct()
-        cache.cache.raw_client.sadd(users_key, *[dct['username'] for dct in q])
+        if q:
+            cache.cache.raw_client.sadd(users_key, *[dct['username'] for dct in q])
+        cache.cache.raw_client.delete(lock)
+        return True
+    else:
+        return False
 
 
 def state_city(dct):
     return '%s:%s' % (dct['state'], dct['city'])
+
+
+def new_token():
+    return ''.join(random.choice(string.ascii_letters) for _ in xrange(16))
